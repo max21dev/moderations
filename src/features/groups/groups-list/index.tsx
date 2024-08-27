53dccd693f03a082e2a91a953cfa60adfd89b4f7
max21dev <max21dev@gmail.com>
2024-08-27 23:10:06 +0200

    add groups data table

--- src/features/groups/groups-list/index.tsx
+++ src/features/groups/groups-list/index.tsx
@@ -1,7 +1,13 @@
 import { useGroupsList } from './hooks';
+import { columns } from '@/features/groups/groups-list/columns';
+import { DataTable } from '@/features/groups/groups-list/data-table.tsx';
 
 export const GroupsList = () => {
   const { groups } = useGroupsList();
 
-  return groups.map((group) => <div key={group.id}>{group.id}</div>);
+  return (
+    <div className="container mx-auto py-10">
+      {groups && <DataTable columns={columns} data={groups} />}
+    </div>
+  );
 };
--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -3,3 +3,6 @@ export { useZapModalState } from './use-zap-modal-state';
 export { useGlobalNdk } from './use-global-ndk';
 export { useGlobalProfile } from './use-global-profile';
 export { useGroups } from './use-groups';
+export { useGroup } from './use-group';
+export { useGroupAdmins } from './use-group-admins';
+export { useGroupMembers } from './use-group-members';
