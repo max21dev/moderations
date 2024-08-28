55c65617934fd62f718ca0d52847ebd0f6de64c7
sepehr-safari <safari.sepehr@gmail.com>
2024-08-28 19:33:58 +0330

    add groups list feature to groups page

--- src/pages/relays/relay/groups/index.tsx
+++ src/pages/relays/relay/groups/index.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
 
+import { GroupsList } from '@/features/groups';
+
 export const GroupsPage = () => {
   return (
     <div>
       <h1>Groups Page</h1>
+
+      <GroupsList />
 
       <Outlet />
     </div>
