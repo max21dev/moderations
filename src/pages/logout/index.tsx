1721c97bc13b5c60872c08662d130a6e86590b08
sepehr-safari <safari.sepehr@gmail.com>
2024-09-10 01:09:14 +0330

    update logout page

--- src/pages/logout/index.tsx
+++ src/pages/logout/index.tsx
@@ -2,8 +2,13 @@ import { useLogin } from 'nostr-hooks';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { useGlobalNdk } from '@/shared/hooks';
+
 export const LogoutPage = () => {
-  const { logout } = useLogin();
+  const { globalNdk, setGlobalNdk } = useGlobalNdk();
+
+  const { logout } = useLogin({ customNdk: globalNdk, setCustomNdk: setGlobalNdk });
+
   const navigate = useNavigate();
 
   useEffect(() => {
