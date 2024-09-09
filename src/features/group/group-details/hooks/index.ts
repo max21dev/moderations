23b90a92b1f2f3cb0c230364e30a63b0fc1b8963
sepehr-safari <safari.sepehr@gmail.com>
2024-09-10 02:28:18 +0330

    fix use-subscribe memo

--- src/features/group/group-details/hooks/index.ts
+++ src/features/group/group-details/hooks/index.ts
@@ -1,6 +1,6 @@
 import { useGroup, useGroupAdmins, useGroupMembers } from '@/shared/hooks';
 
-export const useGroupDetails = ({ groupId }: { groupId: string }) => {
+export const useGroupDetails = ({ groupId }: { groupId: string | undefined }) => {
   const { group } = useGroup(groupId);
   const { members } = useGroupMembers(groupId);
   const { admins } = useGroupAdmins(groupId);
