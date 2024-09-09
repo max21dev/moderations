7cc3b2b56afdaf20b530ef34f899ef3f74fd7bef
sepehr-safari <safari.sepehr@gmail.com>
2024-09-10 01:10:03 +0330

    handle 2 separate ndk instances

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,28 @@
+import { useLogin } from 'nostr-hooks';
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from '@/app/router';
+
+import { useGlobalNdk } from '@/shared/hooks';
 
 import { ThemeProvider } from '@/shared/components/theme-provider';
 
 import './index.css';
 
 export const App = () => {
+  const { globalNdk, setGlobalNdk } = useGlobalNdk();
+
+  useEffect(() => {
+    globalNdk.connect();
+  }, [globalNdk]);
+
+  const { loginFromLocalStorage } = useLogin({ customNdk: globalNdk, setCustomNdk: setGlobalNdk });
+
+  useEffect(() => {
+    loginFromLocalStorage();
+  }, [loginFromLocalStorage]);
+
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
