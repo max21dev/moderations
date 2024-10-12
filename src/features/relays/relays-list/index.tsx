0d7058b16abe1a8cfe7ba6a8c5677be5412d0285
sepehr-safari <safari.sepehr@gmail.com>
2024-10-12 13:06:19 +0330

    fix relays list ui

--- src/features/relays/relays-list/index.tsx
+++ src/features/relays/relays-list/index.tsx
@@ -10,7 +10,6 @@ import {
   TableHeader,
   TableRow,
 } from '@/shared/components/ui/table';
-import { H3 } from '@/shared/components/ui/typography/h3';
 
 import { useStore } from '@/shared/store';
 
@@ -24,39 +23,35 @@ export const RelaysList = () => {
   };
 
   return (
-    <>
-      <H3>My Relays</H3>
-
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">ADDRESS</TableHead>
-            <TableHead>STATUS</TableHead>
-            <TableHead>ABOUT</TableHead>
-            <TableHead className="text-right"></TableHead>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">ADDRESS</TableHead>
+          <TableHead>STATUS</TableHead>
+          <TableHead>ABOUT</TableHead>
+          <TableHead className="text-right"></TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {relays.map((relay) => (
+          <TableRow key={relay}>
+            <TableCell className="font-medium">{relay}</TableCell>
+            <TableCell>-</TableCell>
+            <TableCell>-</TableCell>
+            <TableCell className="text-right">
+              <Button className="ml-2" variant="outline" onClick={() => handleRelayClick(relay)}>
+                <MessagesSquare className="h-5 w-5 mr-2" /> Groups
+              </Button>
+              <Button className="ml-2" variant="outline" size="icon">
+                <Edit className="h-5 w-5" />
+              </Button>
+              <Button className="ml-2" variant="destructive" size="icon">
+                <Trash className="h-5 w-5" />
+              </Button>
+            </TableCell>
           </TableRow>
-        </TableHeader>
-        <TableBody>
-          {relays.map((relay) => (
-            <TableRow key={relay}>
-              <TableCell className="font-medium">{relay}</TableCell>
-              <TableCell>-</TableCell>
-              <TableCell>-</TableCell>
-              <TableCell className="text-right">
-                <Button className="ml-2" variant="outline" onClick={() => handleRelayClick(relay)}>
-                  <MessagesSquare className="h-5 w-5 mr-2" /> Groups
-                </Button>
-                <Button className="ml-2" variant="outline" size="icon">
-                  <Edit className="h-5 w-5" />
-                </Button>
-                <Button className="ml-2" variant="destructive" size="icon">
-                  <Trash className="h-5 w-5" />
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </>
+        ))}
+      </TableBody>
+    </Table>
   );
 };
