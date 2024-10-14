1efae5ac61fdbbaeeb38cbba89be28703c2624d2
sepehr-safari <safari.sepehr@gmail.com>
2024-10-14 15:39:00 +0330

    add relay information as a component to relays list

--- src/features/relays/relays-list/index.tsx
+++ src/features/relays/relays-list/index.tsx
@@ -1,57 +1,15 @@
-import { Edit, MessagesSquare, Trash } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
-
-import { Button } from '@/shared/components/ui/button';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/shared/components/ui/table';
-
 import { useRelaysList } from '@/shared/hooks';
+
+import { RelaysListItem } from './relays-list-item';
 
 export const RelaysList = () => {
   const { relays } = useRelaysList();
 
-  const navigate = useNavigate();
-
-  const handleRelayClick = (relay: string) => {
-    navigate(`/relays/${encodeURIComponent(relay)}/groups/`);
-  };
-
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">URL</TableHead>
-          <TableHead>STATUS</TableHead>
-          <TableHead>ABOUT</TableHead>
-          <TableHead className="text-right"></TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {relays.map((relay) => (
-          <TableRow key={relay}>
-            <TableCell className="font-medium">{relay}</TableCell>
-            <TableCell>-</TableCell>
-            <TableCell>-</TableCell>
-            <TableCell className="text-right">
-              <Button className="ml-2" variant="outline" onClick={() => handleRelayClick(relay)}>
-                <MessagesSquare className="h-5 w-5 mr-2" /> Groups
-              </Button>
-              <Button className="ml-2" variant="outline" size="icon">
-                <Edit className="h-5 w-5" />
-              </Button>
-              <Button className="ml-2" variant="destructive" size="icon">
-                <Trash className="h-5 w-5" />
-              </Button>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <div className="flex flex-col w-full gap-4">
+      {relays.map((relay) => (
+        <RelaysListItem key={relay} relay={relay} />
+      ))}
+    </div>
   );
 };
