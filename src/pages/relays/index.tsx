4a141e2a71819b06d4770ae0fef9b139e009a3b4
sepehr-safari <safari.sepehr@gmail.com>
2024-10-16 10:23:25 +0330

    handle loading state for relays page

--- src/pages/relays/index.tsx
+++ src/pages/relays/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 
+import { Spinner } from '@/shared/components/spinner';
 import { Button } from '@/shared/components/ui/button';
 import { H3 } from '@/shared/components/ui/typography/h3';
 
@@ -8,9 +9,11 @@ import { useRelaysList } from '@/shared/hooks';
 import { EmptyRelays, RelaysList } from '@/features/relays';
 
 export const RelaysPage = () => {
-  const { relays } = useRelaysList();
+  const { relays, isLoading } = useRelaysList();
 
-  if (relays.length == 0) return <EmptyRelays />;
+  if (isLoading) return <Spinner />;
+
+  if (relays.length === 0) return <EmptyRelays />;
 
   return (
     <>
