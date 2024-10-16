f26511d8a0e2d19bb9525226c8ebbd231760b6cc
sepehr-safari <safari.sepehr@gmail.com>
2024-10-16 13:41:45 +0330

    refactor relays list components

--- src/features/relays/index.ts
+++ src/features/relays/index.ts
@@ -1,2 +1,1 @@
-export * from './empty-relays';
 export * from './relays-list';
--- src/pages/relays/index.tsx
+++ src/pages/relays/index.tsx
@@ -1,16 +1,11 @@
 import { Link } from 'react-router-dom';
 
-import { Spinner } from '@/shared/components/spinner';
 import { Button } from '@/shared/components/ui/button';
 import { H3 } from '@/shared/components/ui/typography/h3';
 
-import { useRelaysList } from '@/shared/hooks';
-
-import { EmptyRelays, RelaysList } from '@/features/relays';
+import { RelaysList } from '@/features/relays';
 
 export const RelaysPage = () => {
-  const { relays, isLoading } = useRelaysList();
-
   return (
     <>
       <div className="mb-4 w-full flex items-center">
@@ -21,7 +16,7 @@ export const RelaysPage = () => {
         </Button>
       </div>
 
-      {isLoading ? <Spinner /> : relays.length == 0 ? <EmptyRelays /> : <RelaysList />}
+      <RelaysList />
     </>
   );
 };
