607070672269c8b313fd0d8fc96ca281b18c682c
sepehr-safari <safari.sepehr@gmail.com>
2024-10-17 23:47:27 +0330

    enhance use relay information hook

--- src/shared/hooks/use-relay-information/index.ts
+++ src/shared/hooks/use-relay-information/index.ts
@@ -1,10 +1,12 @@
 import { fetchRelayInformation, RelayInformation } from 'nostr-tools/nip11';
 import { useEffect, useState } from 'react';
 
-export const useRelayInformation = ({ relay }: { relay: string }) => {
+export const useRelayInformation = ({ relay }: { relay: string | undefined }) => {
   const [info, setInfo] = useState<RelayInformation | undefined>(undefined);
 
   useEffect(() => {
+    if (!relay) return;
+
     fetchRelayInformation(relay).then((info) => {
       setInfo(info);
     });
