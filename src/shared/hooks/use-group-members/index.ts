2e70c7b31203fdb087349d02501268224ea9698b
sepehr-safari <safari.sepehr@gmail.com>
2024-10-24 23:31:00 +0330

    refactor group admins and members hooks

--- src/shared/hooks/use-group-members/index.ts
+++ src/shared/hooks/use-group-members/index.ts
@@ -1,23 +1,10 @@
 import { NDKKind } from '@nostr-dev-kit/ndk';
-import { useSubscribe } from 'nostr-hooks';
 import { useMemo } from 'react';
 
-import { useNip29Ndk } from '@/shared/hooks';
+import { useGroupEvents } from '@/shared/hooks';
 
 export const useGroupMembers = (groupId: string | undefined) => {
-  const { nip29Ndk } = useNip29Ndk();
-
-  const { events } = useSubscribe(
-    useMemo(
-      () => ({
-        filters: !groupId ? [] : [{ kinds: [39002 as NDKKind], '#d': [groupId], limit: 1 }],
-        enabled: !!groupId,
-        opts: { groupable: false },
-        customNdk: nip29Ndk,
-      }),
-      [groupId, nip29Ndk],
-    ),
-  );
+  const { events } = useGroupEvents(groupId, NDKKind.GroupMembers);
 
   const members = useMemo(
     () =>
