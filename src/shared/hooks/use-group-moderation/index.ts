e29548c60f8d6e846adbed0367dfbd8b63f914aa
sepehr-safari <safari.sepehr@gmail.com>
2024-10-31 21:43:52 +0330

    implement update metadata

--- src/shared/hooks/use-group-moderation/index.ts
+++ src/shared/hooks/use-group-moderation/index.ts
@@ -1,9 +1,11 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import { NDKKind } from '@nostr-dev-kit/ndk';
 import { useNewEvent } from 'nostr-hooks';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { useToast } from '@/shared/components/ui/use-toast';
 
-import { useGroup, useNip29Ndk } from '@/shared/hooks';
+import { useGroupMetadata, useNip29Ndk } from '@/shared/hooks';
+import { GroupMetadata } from '@/shared/types';
 
 export const useGroupModeration = ({ groupId }: { groupId: string | undefined }) => {
   const { toast } = useToast();
@@ -14,9 +16,16 @@ export const useGroupModeration = ({ groupId }: { groupId: string | undefined })
 
   const { nip29Ndk } = useNip29Ndk();
 
-  const { group } = useGroup(groupId);
+  const { metadataEvent } = useGroupMetadata(groupId);
 
   const { createNewEvent } = useNewEvent({ customNdk: nip29Ndk });
+
+  const toastSuccess = ({ message }: { message: string }) => {
+    toast({
+      title: 'Success',
+      description: message,
+    });
+  };
 
   const toastError = ({ message }: { message: string }) => {
     toast({
@@ -27,7 +36,7 @@ export const useGroupModeration = ({ groupId }: { groupId: string | undefined })
   };
 
   const deleteGroup = () => {
-    if (!groupId || !group) return;
+    if (!groupId || !metadataEvent) return;
 
     const event = createNewEvent();
     event.kind = 9008;
@@ -35,7 +44,7 @@ export const useGroupModeration = ({ groupId }: { groupId: string | undefined })
     event.publish().then(
       (r) => {
         if (r.size > 0) {
-          nip29Ndk.cacheAdapter?.deleteEvent?.(group.event);
+          nip29Ndk.cacheAdapter?.deleteEvent?.(metadataEvent);
 
           navigate(`/relays/${encodeURIComponent(relay || '')}/groups/`, { replace: true });
         } else {
@@ -48,7 +57,36 @@ export const useGroupModeration = ({ groupId }: { groupId: string | undefined })
     );
   };
 
+  const updateMetadata = (metadata: Omit<GroupMetadata, 'id'>) => {
+    if (!groupId) return;
+
+    const event = createNewEvent();
+    event.kind = NDKKind.GroupAdminEditMetadata;
+    event.tags = [
+      ['h', groupId],
+      ['name', metadata.name],
+      ['about', metadata.about],
+      ['picture', metadata.picture],
+      [metadata.isPublic ? 'public' : 'private'],
+      [metadata.isOpen ? 'open' : 'closed'],
+    ];
+
+    event.publish().then(
+      (r) => {
+        if (r.size > 0) {
+          toastSuccess({ message: 'Group metadata updated' });
+        } else {
+          toastError({ message: 'Failed to delete group' });
+        }
+      },
+      () => {
+        toastError({ message: 'Failed to delete group' });
+      },
+    );
+  };
+
   return {
     deleteGroup,
+    updateMetadata,
   };
 };
