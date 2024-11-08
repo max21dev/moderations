bc00494eab547fee1f228ee857988a3f0d77af40
sepehr-safari <safari.sepehr@gmail.com>
2024-11-08 13:49:43 +0330

    add details for user info row in group admins & details

--- src/features/groups/group-admins/index.tsx
+++ src/features/groups/group-admins/index.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 
 import { CardContainer } from '@/shared/components/card-container';
+import { Badge } from '@/shared/components/ui/badge';
 
 import { UserInfoRow } from '@/features/users';
 
@@ -18,7 +19,17 @@ export const GroupAdmins = () => {
       <div className="flex flex-col gap-4">
         <CardContainer title={`Admins (${admins.length})`}>
           {admins.map((admin) => (
-            <UserInfoRow key={admin.publicKey} pubkey={admin.publicKey} />
+            <div className="flex items-center gap-2">
+              <UserInfoRow key={admin.publicKey} pubkey={admin.publicKey}>
+                <div className="flex gap-2 flex-wrap">
+                  {admin.roles.map((role) => (
+                    <Badge variant="outline" className="shrink-0">
+                      {role}
+                    </Badge>
+                  ))}
+                </div>
+              </UserInfoRow>
+            </div>
           ))}
         </CardContainer>
       </div>
