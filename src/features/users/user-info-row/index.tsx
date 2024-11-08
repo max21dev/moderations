8df76e97a3f92326145462718ea307447c13ad4a
sepehr-safari <safari.sepehr@gmail.com>
2024-11-08 13:26:51 +0330

    enhance user info row

--- src/features/users/user-info-row/index.tsx
+++ src/features/users/user-info-row/index.tsx
@@ -1,4 +1,8 @@
+import { ChevronUpIcon } from 'lucide-react';
+import { useState } from 'react';
+
 import { Avatar, AvatarFallback, AvatarImage } from '@/shared/components/ui/avatar';
+import { Button } from '@/shared/components/ui/button';
 import {
   Tooltip,
   TooltipContent,
@@ -6,41 +10,66 @@ import {
   TooltipTrigger,
 } from '@/shared/components/ui/tooltip';
 
-import { ellipsis } from '@/shared/utils';
+import { cn, ellipsis } from '@/shared/utils';
 
 import { useUserInfoRow } from './hooks';
 
-export function UserInfoRow({ pubkey }: { pubkey: string }) {
+export function UserInfoRow({ pubkey, children }: { pubkey: string; children?: React.ReactNode }) {
   const { name, image, npub } = useUserInfoRow({ pubkey });
 
-  return (
-    <a
-      className="flex items-center gap-4 p-2 group"
-      href={`https://njump.me/${npub}`}
-      target="_blank"
-      rel="noreferrer"
-    >
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Avatar className="flex justify-center items-center">
-              {!image ? (
-                <AvatarFallback>{pubkey.slice(0, 2).toUpperCase()}</AvatarFallback>
-              ) : (
-                <AvatarImage src={image} alt={name} />
-              )}
-            </Avatar>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>{name}</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false);
 
-      <div className="flex flex-col group-hover:underline">
-        <span className="text-sm">{name}</span>
-        <span className="text-sm text-gray-500">{ellipsis(npub, 10)}</span>
+  return (
+    <div className="group p-2 w-full rounded-lg border border-transparent hover:border-muted-foreground transition-colors duration-500 ease-out">
+      <div className="flex items-center gap-4 w-full">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <a href={`https://njump.me/${npub}`} target="_blank" rel="noreferrer">
+                <Avatar asChild className="flex justify-center items-center">
+                  {!image ? (
+                    <AvatarFallback>{pubkey.slice(0, 2).toUpperCase()}</AvatarFallback>
+                  ) : (
+                    <AvatarImage src={image} alt={name} />
+                  )}
+                </Avatar>
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{name}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        <a
+          className="flex flex-col group-hover:underline"
+          href={`https://njump.me/${npub}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="text-sm">{name}</span>
+          <span className="text-sm text-gray-500">{ellipsis(npub, 10)}</span>
+        </a>
+
+        {children && (
+          <Button
+            variant="ghost"
+            size="icon"
+            className="ml-auto rounded-full"
+            onClick={() => setIsDetailsOpen((prev) => !prev)}
+          >
+            <ChevronUpIcon
+              size={16}
+              className={cn(
+                'transition-all duration-500 ease-out text-background group-hover:text-muted-foreground',
+                isDetailsOpen && 'transform rotate-180',
+              )}
+            />
+          </Button>
+        )}
       </div>
-    </a>
+
+      {children && <div className={cn('mt-2', isDetailsOpen ? 'block' : 'hidden')}>{children}</div>}
+    </div>
   );
 }
