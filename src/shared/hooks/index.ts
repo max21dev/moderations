ea766027bba162243baa9a7611ab09d205ab82dc
sepehr-safari <safari.sepehr@gmail.com>
2024-11-28 11:33:50 +0330

    update with new nostr-hooks, remove all nip29 types

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,10 +1,7 @@
 export { useActiveGroupId } from './use-active-group-id';
 export { useActiveRelay } from './use-active-relay';
 export { useGroupModeration } from './use-group-moderation';
-export { useGroupNotes } from './use-group-notes';
-export { useGroupsMetadataList } from './use-groups-metadata-list';
 export { useLoginModalState } from './use-login-modal-state';
-export { useNip29Ndk } from './use-nip29-ndk';
 export { useRelayInformation } from './use-relay-information';
 export { useRelaysList } from './use-relays-list';
 export { useZapModalState } from './use-zap-modal-state';
